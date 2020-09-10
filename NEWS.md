62789f58f52fd052e68a8d94cf7a36c8efc1e981
mrustl <michael.rustler@kompetenz-wasser.de>
2020-09-10 11:02:43 +0200

    team: add vignette and try gh-actions scraping

--- NEWS.md
+++ NEWS.md
@@ -1,10 +1,10 @@
 # kwb.site 0.0.0.9000
 
-* add functions get_project() and get_project_urls() for getting projects infos
+* Add function `get_team()` for scraping team infos
+
+* Add functions `get_project()` and `get_project_urls()` for getting projects infos
  from KWB website (https://kompetenz-wasser.de)
 
 * Added a `NEWS.md` file to track changes to the package.
 
-* see https://style.tidyverse.org/news.html for writing a good `NEWS.md`
-
-
+* See https://style.tidyverse.org/news.html for writing a good `NEWS.md`
--- R/get_team.R
+++ R/get_team.R
@@ -1,33 +1,74 @@
-if(FALSE) {
+#' RSelenium Connection
+#'
+#' @return connection to R Selenium driver
+#' @export
+#' @importFrom RSelenium rsDriver
+rs_connect <- function() {
 
-library(rvest)
+ rs_serverclient <- RSelenium::rsDriver(browser = "firefox", geckover = "0.25.0")
+ rs_serverclient$client
+}
 
+#' Get Team HTML
+#'
+#' @param url url to team site (default:  "https://www.kompetenz-wasser.de/de/ueber-uns/team-2/")
+#' @param rs connection to RSelenium (kwb.site::rs_connect())
+#'
+#' @return html source code of team website (after clicking "load-more" and
+#' "open-details" buttons)
+#' @export
+#' @importFrom  xml2 read_html
+#' @examples
+#'  \dontrun{
+#'  rs <- rs_connect(browser = "firefox", geckover = "0.25.0")
+#'  team_html <- kwb.site::get_team_html(rs = rs)
+#'  team_html
+#'  }
 get_team_html <- function(url =  "https://www.kompetenz-wasser.de/de/ueber-uns/team-2/",
                           rs) {
 
 
 
-  rs$client$open()
-  rs$client$navigate(url)
+  rs$open()
+  rs$navigate(url)
 
-  cookie <- rs$client$findElement(using = "xpath", value = '//*[@id="cn-accept-cookie"]')
+  cookie <- rs$findElement(using = "xpath", value = '//*[@id="cn-accept-cookie"]')
   message("Click on cookie")
   cookie$clickElement()
     for(i in 1:4) {
-      button_loadmore <- rs$client$findElement(using = "xpath", value = "//button[@class='load-more']")
+      button_loadmore <- rs$findElement(using = "xpath", value = "//button[@class='load-more']")
+      message(sprintf("Click on load more button: %d", i))
       button_loadmore$clickElement()
       Sys.sleep(3)
     }
 
-  button_opendetails <- rs$client$findElements(using = "xpath", value = '//*[contains(concat( " ", @class, " " ), concat( " ", "open-hidden-smooth-button", " " ))]')
+  button_opendetails <- rs$findElements(using = "xpath", value = '//*[contains(concat( " ", @class, " " ), concat( " ", "open-hidden-smooth-button", " " ))]')
 
   sapply( button_opendetails, function(x) x$clickElement())
 
-  team_html <- xml2::read_html(rs$client$getPageSource()[[1]])
-  rs$client$close()
+  team_html <- xml2::read_html(rs$getPageSource()[[1]])
+  rs$close()
   team_html
 }
 
+#' Team: get person
+#'
+#' @param x one element of css-class="li-employee-item" (as retrieved by
+#' kwb.site::get_team_url())
+#'
+#' @return tibble with information of one person
+#' @export
+#' @importFrom rvest html_node html_nodes html_attr html_text
+#' @importFrom stringr str_remove_all str_trim
+#' @examples
+#' #' @examples
+#'  \dontrun{
+#'  rs <- kwb.site::rs_connect()
+#'  team_html <- kwb.site::get_team_html(rs = rs)
+#'  team <- team_html %>%
+#'  rvest::html_nodes("li.employee-item")
+#'  kwb.site::get_person(team[1])
+#'  }
  get_person <- function(x) {
       avatar_url <- x %>%
       rvest::html_node("div.headshot") %>%
@@ -106,8 +147,25 @@ get_team_html <- function(url =  "https://www.kompetenz-wasser.de/de/ueber-uns/t
 
 }
 
+ #' Team: get all team members
+ #'
+ #' @param url url to team site (default:  "https://www.kompetenz-wasser.de/de/ueber-uns/team-2/")
+ #' @param rs connection to RSelenium (kwb.site::rs_connect()))
+ #'
+ #' @return tibble with information of all persons
+ #' @export
+ #' @importFrom tibble tibble
+ #' @importFrom dplyr bind_cols bind_rows
+ #' @importFrom rvest html_nodes
+ #'
+ #' @examples
+ #' #' @examples
+ #'  \dontrun{
+ #'  rs <- kwb.site::rs_connect()
+ #'  kwb.site::get_team(rs = rs)
+ #'  }
 
- get_persons <- function(url = "https://www.kompetenz-wasser.de/de/ueber-uns/team-2/",
+ get_team <- function(url = "https://www.kompetenz-wasser.de/de/ueber-uns/team-2/",
                          rs) {
 
    team_html <- get_team_html(url, rs)
@@ -116,15 +174,10 @@ get_team_html <- function(url =  "https://www.kompetenz-wasser.de/de/ueber-uns/t
      rvest::html_nodes("li.employee-item")
 
 
-   lapply(team, function(x) get_person(x)) %>%
-     dplyr::bind_rows()
+   dplyr::bind_cols(tibble::tibble(url = url),
+                    lapply(team, function(x) get_person(x)) %>%
+                    dplyr::bind_rows())
 
  }
 
 
- rs <- RSelenium::rsDriver(browser = "firefox", geckover = "0.25.0")
-
- team_de <- get_persons(url = "https://www.kompetenz-wasser.de/de/ueber-uns/team-2/", rs)
- team_en <- get_persons(url = "https://www.kompetenz-wasser.de/en/ueber-uns/team-2/", rs)
-
-team_de %>% tidyr::unnest(projects)
